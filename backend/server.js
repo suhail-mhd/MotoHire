4e0bf56914c17f0eff2265a552d1caf81cef9e2b
Mohamed Suhail <suhailmhd070@gmail.com>
2024-07-24 11:52:46 +0530

    deployment path issue solved

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,13 @@ app.use("/api/admin", adminRoute);
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
+  const buildPath = path.join(__dirname, "frontend", "build");
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.use(express.static(buildPath));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(buildPath, "index.html"));
+    });
 } else {
   app.get("/", (req, res) => {
     res.status(200).send("Hellow Everybody..asdas..");
