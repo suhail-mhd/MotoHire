c0dcd112fd996f99adf3954b1d9f2380ac735ffb
Mohamed Suhail <suhailmhd070@gmail.com>
2024-08-13 15:40:17 +0530

    pre-loader created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React, { useState, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
+import Loading from "./components/Loading/Loading";
 
 function App() {
-  return <Layout />;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust time as needed
+  }, []);
+
+  return <>{loading ? <Loading /> : <Layout />}</>;
 }
 
 export default App;
--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import CarLoader from '../../assets/all-images/cars-img/9e9c68435731c23c00573a1544d539b3.gif'
 import './Loading.css'
 
 function Loading() {
   return (
     <div id="root">
     <div class="loader-wrapper">
-      <div class="loader"></div>
+      {/* <div class="loader"></div> */}
+      <img src={CarLoader} alt="" />
     </div>
   </div>
   )
